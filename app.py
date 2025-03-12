fee68651df239c120fa1dd0aa2e0365f003f3a34
sn1p2r78 <117768713+sn1p2r78@users.noreply.github.com>
2025-03-12 17:11:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def index():
                 start0 = datetime.datetime.now(datetime.timezone.utc)
                 time.sleep(40)
             
-                start = start0.strftime("%Y-%m-%dT%H:00:00.999Z")
-                end = start0.strftime("%Y-%m-%dT24:60:60.999Z")
+                start = start0.strftime("%Y-%m-%dT%H:00:00")
+                end = start0.strftime("%Y-%m-%dT24:60:60")
                 headers = {
                     'Content-Type': 'application/json',
                     'Api-Key': key,
